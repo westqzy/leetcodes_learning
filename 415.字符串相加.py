3409bc8e15be96ce07fdfa3f4c2af46d6681ad8b
westqzy <997673296@qq.com>
2020-11-05 18:44:57 +0800

    哈哈

---
+++ 415.字符串相加.py
@@ -0,0 +1,22 @@
+'''
+Author: your name
+Date: 2020-11-05 15:47:45
+LastEditTime: 2020-11-05 18:44:00
+LastEditors: Please set LastEditors
+Description: In User Settings Edit"
+FilePath: \leetcodes\415.字符串相加.py
+'''
+#
+# @lc app=leetcode.cn id=415 lang=python3
+#
+# [415] 字符串相加
+#
+
+# @lc code=start
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        
+# @lc code=end
+
+print("aaaaa")
+print("aaaaaaaaaaaaa")
