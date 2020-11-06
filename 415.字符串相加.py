88420f910843647612bcbaf56c3bee61c2cc7d44
westqzy <997673296@qq.com>
2020-11-06 13:29:18 +0800

    116

--- 415.字符串相加.py
+++ 415.字符串相加.py
@@ -1,15 +1,4 @@
-'''
-Author: your name
-Date: 2020-11-05 15:47:45
-<<<<<<< HEAD
-LastEditTime: 2020-11-06 12:53:21
-=======
-LastEditTime: 2020-11-05 20:38:19
->>>>>>> 645284cda560924d8e66a7d86e6135f131da1728
-LastEditors: Please set LastEditors
-Description: In User Settings Edit"
-FilePath: \leetcodes\415.字符串相加.py
-'''
+
 #
 # @lc app=leetcode.cn id=415 lang=python3
 #
@@ -19,14 +8,5 @@ FilePath: \leetcodes\415.字符串相加.py
 # @lc code=start
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        
 # @lc code=end
 
-<<<<<<< HEAD
-print("aaaaa")
-print("aaaaaaaaaaaaa")
-
-qiziyue wo ai ni 
-=======
-print("iamyoudaddy")
->>>>>>> 645284cda560924d8e66a7d86e6135f131da1728
