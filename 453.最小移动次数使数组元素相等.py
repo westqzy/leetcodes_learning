83da974d12727b7bbf9360f941d3226c33e3b479
westqzy <997673296@qq.com>
2020-11-09 20:16:10 +0800

    119

---
+++ 453.最小移动次数使数组元素相等.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=453 lang=python3
+#
+# [453] 最小移动次数使数组元素相等
+#
+
+# @lc code=start
+class Solution:
+    def minMoves(self, nums: List[int]) -> int:
+        add_new = len(nums) - 1
+        count = 0
+        for i in nums:
+            count += i
+        while 
+# @lc code=end
+
