6e4a06c4a9877d98ccda4410b36c19271f1b3fee
westqzy <997673296@qq.com>
2020-11-10 20:46:08 +0800

    1110

---
+++ 461.汉明距离.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode.cn id=461 lang=python3
+#
+# [461] 汉明距离
+#
+
+# @lc code=start
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        count = 0
+        x_bin = bin(x)
+        y_bin = bin(y)
+        x_bin = x_bin[2:].zfill(32)
+        y_bin = y_bin[2:].zfill(32)
+        print(y_bin)
+        for i in range(len(x_bin)):
+            if x_bin[i] != y_bin[i]:
+                count += 1
+        return count
+# @lc code=end
+
+x_bin = bin(10)
+print(x_bin)
+x_bin = x_bin[2:].zfill(32)
+print(x_bin)
