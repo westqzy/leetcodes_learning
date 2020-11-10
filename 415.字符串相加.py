4bf5b9219dc2e48363d06d79b25d9a744cec4367
westqzy <997673296@qq.com>
2020-11-10 14:56:40 +0800

    1

--- 415.字符串相加.py
+++ 415.字符串相加.py
@@ -1,11 +1,7 @@
 '''
 Author: your name
 Date: 2020-11-05 15:47:45
-<<<<<<< HEAD
-LastEditTime: 2020-11-06 12:53:21
-=======
-LastEditTime: 2020-11-05 20:38:19
->>>>>>> 645284cda560924d8e66a7d86e6135f131da1728
+LastEditTime: 2020-11-06 13:01:53
 LastEditors: Please set LastEditors
 Description: In User Settings Edit"
 FilePath: \leetcodes\415.字符串相加.py
@@ -22,11 +18,4 @@ class Solution:
         
 # @lc code=end
 
-<<<<<<< HEAD
-print("aaaaa")
-print("aaaaaaaaaaaaa")
-
-qiziyue wo ai ni 
-=======
 print("iamyoudaddy")
->>>>>>> 645284cda560924d8e66a7d86e6135f131da1728
